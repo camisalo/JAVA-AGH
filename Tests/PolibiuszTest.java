0bab65001c0faedafcf7eee1060ab9bc4f250bf9
camisalo <23015249+camisalo@users.noreply.github.com>
2017-10-17 22:41:56 +0200

    Add files via upload
    test

---
+++ Tests/PolibiuszTest.java
@@ -0,0 +1,21 @@
+import lab4.zad5.Polibiusz;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PolibiuszTest {
+    @Test
+    void crypt() {
+        Polibiusz P = new Polibiusz();
+        String word = "Rocky";
+        assertEquals("42 34 13 25 54 ", P.crypt(word));
+    }
+
+    @Test
+    void decrypt() {
+        Polibiusz P = new Polibiusz();
+        String crypted = "42 34 13 25 54 ";
+        assertEquals("rocky", P.decrypt(crypted));
+    }
+
+}
