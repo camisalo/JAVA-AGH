ea92b35cb4ae4b4bb10cf0bca3f2fa0d1f07741a
camisalo <konrad.gebczynski@gmail.com>
2017-10-18 13:04:30 +0200

    1

--- src/lab3/zad5/EmailTest.java
+++ src/lab3/zad5/EmailTest.java
@@ -3,8 +3,8 @@ package lab3.zad5;
 public class EmailTest {
     public static void main(String[] args)  {
         EmailMessage wiadomosc = EmailMessage.builder()
-                .addFrom("camisalo94@gmail.com")
-                .addTo("camisalo94@gmail.com")
+                .addFrom("")
+                .addTo("")
                 .addSubject("Mail testowy")
                 .addContent("Jakaś tam treść.")
                 .build();
