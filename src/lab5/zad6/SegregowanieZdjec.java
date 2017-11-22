1579af225c25f419607725b0688767ce529471ae
camisalo <camisalo@student.agh.edu.pl>
2017-11-22 13:13:13 +0100

    ehh

--- src/lab5/zad6/SegregowanieZdjec.java
+++ src/lab5/zad6/SegregowanieZdjec.java
@@ -3,6 +3,7 @@ package lab5.zad6;
 import io.indico.*;
 import io.indico.api.results.IndicoResult;
 import io.indico.api.utils.IndicoException;
+import lab6.zad3.Przeg_zdjecia;
 import org.apache.commons.io.FileUtils;
 
 
@@ -15,8 +16,11 @@ import java.nio.file.StandardCopyOption;
 import java.util.*;
 
 public class SegregowanieZdjec {
+    private Map<String,Double> wyniki = new TreeMap<>();
+    private String patch;
+
     public SegregowanieZdjec(String patch) throws IndicoException, IOException {
-        // single example
+        this.patch = patch;
 
         Indico indico = new Indico("a16f7a067abf922404162ad6a6064bfa");
 
@@ -27,8 +31,10 @@ public class SegregowanieZdjec {
             IndicoResult single = indico.imageRecognition.predict(f);
             Map<String, Double> result = single.getImageRecognition();
 
+            double wynik = Collections.max(result.entrySet(), Map.Entry.comparingByValue()).getValue();
             String key = Collections.max(result.entrySet(), Map.Entry.comparingByValue()).getKey();
 
+            wyniki.put(key,licznik + wynik);
 
             File dir = new File(patch + "\\" + key);
             if (!dir.mkdir()){
@@ -38,6 +44,16 @@ public class SegregowanieZdjec {
             System.out.println(f.getPath());
             Files.copy(Paths.get(f.getPath()), Paths.get(patch + "\\" + key + "\\" + licznik + key + ".jpg"), StandardCopyOption.REPLACE_EXISTING);
             ++licznik;
+        }
+
+        Przeg_zdjecia();
+    }
+
+    private void Przeg_zdjecia(){
+        File foldery = new File(patch);
+
+        if (!foldery.isDirectory()){
+            System.out.println(patch + " is not directory");
         }
     }
 }
---
+++ src/lab6/zad3/Przeg_zdj_test.java
@@ -0,0 +1,8 @@
+package lab6.zad3;
+
+public class Przeg_zdj_test {
+    public static void main(String[] args) {
+        Przeg_zdjecia P = new Przeg_zdjecia();
+
+    }
+}
